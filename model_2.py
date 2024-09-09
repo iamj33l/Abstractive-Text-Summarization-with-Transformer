601fad66bbe3577294bf2080e002c0d920c5d673
Jeel Patel <99801843+iamj33l@users.noreply.github.com>
2024-09-09 14:47:12 +0530

    Update model_2.py

--- model_2.py
+++ model_2.py
@@ -9,19 +9,20 @@ from torch.nn import CrossEntropyLoss
 import numpy as np
 
 """ Collecting training data """
-print("Collecting data...")
-train_dataset = load_dataset("cnn_dailymail", "3.0.0")
-train_raw_data = train_dataset['train'].to_pandas()
+print("Collectiong data...")
+dataset = load_dataset("cnn_dailymail", "3.0.0")
+
+print("Collecting training data...")
+train_raw_data = dataset['train'].to_pandas()
 train_columns = train_raw_data[['article', 'highlights']]
-train_data = pd.DataFrame(train_columns)
+train_data = pd.DataFrame(train_columns).head(1000)
 train_data.drop_duplicates(subset=['article'], inplace=True)
 print("Done: collected training data")
 
 """ Collecting testing data """
-test_dataset = load_dataset("cnn_dailymail", "3.0.0")
-test_raw_data = test_dataset['test'].to_pandas()
+test_raw_data = dataset['test'].to_pandas()
 test_columns = test_raw_data[['article', 'highlights']]
-test_data = pd.DataFrame(test_columns)
+test_data = pd.DataFrame(test_columns).head(1000)
 test_data.drop_duplicates(subset=['article'], inplace=True)
 print("Done: collected test data")
 
